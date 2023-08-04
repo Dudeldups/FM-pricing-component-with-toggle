c4f1893b5f0f6e5f707cf9466f0f762a12db8af1
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-05 00:16:05 +0200

    Added form and checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function App() {
-  return <div>App</div>;
+  const [isMonthly, setIsMonthly] = useState(true);
+
+  return (
+    <div>
+      <h1 className="sr-only">
+        This is a component from a Frontend Mentor challenge
+      </h1>
+
+      <form className="form" action="#">
+        <fieldset>
+          <legend>
+            <h2 className="form__title">Our Pricing</h2>
+          </legend>
+
+          <div className="toggle">
+            <label className="sr-only" htmlFor="toggleMonthly">
+              Switch to {isMonthly ? "annual" : "monthly"} payment method
+            </label>
+            <input
+              className="toggle__input sr-only"
+              type="checkbox"
+              name="toggleMonthly"
+              id="toggleMonthly"
+              checked={isMonthly}
+            />
+            <span className="toggle__text" aria-hidden="true">
+              Annually
+            </span>
+            <div className="toggle__container"></div>
+            <span className="toggle__text" aria-hidden="true">
+              Monthly
+            </span>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
